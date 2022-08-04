28ea1d0ed2674a1c2edfd971e96a2a6fe18e625f
Akshat Jain <93046152+unthinkable-akshatjain@users.noreply.github.com>
2022-08-04 12:06:11 +0530

    Update backend.tf

--- system_integration_testing/me-south-1/backend.tf
+++ system_integration_testing/me-south-1/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket         = "terraform-backend-state-test-456"
-    dynamodb_table = "terraform-backend-lock-test-456"
+    bucket         = "terraform-backend-test"
+    dynamodb_table = "terraform-backend-lock"
     encrypt        = true
     key            = "ap-south-1/./terraform.tfstate"
     region         = "ap-south-1"
