e972c14b0640965c4d2866c540caf1525841cde7
Akshat Jain <93046152+unthinkable-akshatjain@users.noreply.github.com>
2022-08-04 12:16:32 +0530

    Delete backend.tf

--- system_integration_testing/me-south-1/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "terraform-backend-test"
-    dynamodb_table = "terraform-backend-lock"
-    encrypt        = true
-    key            = "ap-south-1/./terraform.tfstate"
-    region         = "ap-south-1"
-  }
-}
